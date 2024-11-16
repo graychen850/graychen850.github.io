ef11c9e24ef7818260eaffd9342cf696c0d13998
yuyutw123 <yuyutw123@gmail.com>
2024-11-16 22:37:28 +0800

    update banner

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ export const siteConfig: SiteConfig = {
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: true,
+    enable: false,
     src: 'assets/images/icon.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
@@ -30,8 +30,8 @@ export const siteConfig: SiteConfig = {
   },
   favicon: [    // Leave this array empty to use the default favicon
     {
-      src: 'assets/images/icon.jpg',    // Path of the favicon, relative to the /public directory
-      theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      src: 'assets/images/icon.png',    // Path of the favicon, relative to the /public directory
+      theme: 'dark',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
       sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     }
   ]
